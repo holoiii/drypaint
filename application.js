fcbbb7cf411b0353ca9107e7dc66bd4d49a7d02e
Jay Swain <github@jayswain.com>
2012-09-26 15:01:45 -0700

    woot

--- application.js
+++ application.js
@@ -17,38 +17,73 @@
         var lineTwoStart = [tile.attrs.x + tile.attrs.width, tile.attrs.y];
         var lineOne = "M" + tile.attrs.x + " " + tile.attrs.y + "L" + (tile.attrs.x + tile.attrs.width) + " " + (tile.attrs.y + tile.attrs.height);
         var lineTwo = "M" + (tile.attrs.x + tile.attrs.width) + " " + tile.attrs.y + "L" + tile.attrs.x + " " + (tile.attrs.y + tile.attrs.height);
-        return that.paper.path(lineOne + " " + lineTwo);
+        var marker = that.paper.path(lineOne + " " + lineTwo);
+        tile.marker = marker;
+        return marker;
       }
       Raphael.fn.circleMarker = function(tile) {
         var cx = tile.attrs.x + tile.attrs.width / 2;
         var cy = tile.attrs.y + tile.attrs.height / 2;
-        return that.paper.circle(cx, cy, that.options.tileSize / 2);
+        var marker = that.paper.circle(cx, cy, that.options.tileSize / 2);
+        tile.marker = marker;
+        return marker;
       }
     },
     initialize: function() {
+      var that = this;
       var board = new Array(3);
       for (var i = 0; i < 3; i++) {
         board[i] = new Array(3);
       }
       this.board = board;
-      this.paper = Raphael(0, 0, this.options.tileSize * 3, this.options.tileSize * 3);
+      this.paper = Raphael('game-board', this.options.tileSize * 3, this.options.tileSize * 3);
+      this.playerOne = {
+        name: "Player 1",
+        makeMove: function(tile) {
+          if(tile.marker == undefined) {
+            that.paper.circleMarker(tile);
+            that.changePlayers();
+          }
+        }
+      };
+      this.playerTwo = {
+        name: "Player 2",
+        makeMove: function(tile) {
+          if(tile.marker == undefined) {
+            that.paper.checkMarker(tile);
+            that.changePlayers();
+          }
+        }
+      };
+      this.currentPlayer = this.playerOne;
       this.resetGame();
     },
+    changePlayers: function() {
+      if(this.currentPlayer == this.playerOne) {
+        this.currentPlayer = this.playerTwo;
+      } else {
+        this.currentPlayer = this.playerOne;
+      }
+    },
     resetGame: function() {
+      var that = this;
       for (var x = 0; x < 3; x++) {
         for (var y = 0; y < 3; y++) {
           var newTile = this.paper.tile(x, y);
+          newTile.markerPresent = false;
           newTile.click(function() {
-            this.paper.circleMarker(this);
-            this.paper.checkMarker(this);
+            that.currentPlayer.makeMove(this);
+            that.checkForWinner(that.board);
           });
-          console.log(newTile);
           this.board[x][y] = newTile;
         }
       }
     },
     checkForWinner: function(board) {
-
+      console.log("checking for winner!");
     },
+    message: function(text) {
+      $("#message-area").html(text);
+    }
   });
 })(jQuery, _, window.DryPaint = window.DryPaint || {});
