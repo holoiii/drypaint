e86993fc71ed097ac9d1be2f0a8e2eba3755e819
Albert Peng <pair+albert.peng@socialchorus.com>
2012-09-26 09:07:49 -0700

    checkboxes and circles

--- application.js
+++ application.js
@@ -7,8 +7,22 @@
 
   _.extend(d.TicTacToe.prototype, {
     extendRaphael: function() {
+      var that = this;
       Raphael.fn.tile = function(x, y) {
-        return this.paper.rect(x * this.options.tileSize, y * this.options.tileSize, this.options.tileSize, this.options.tileSize);
+        var rect = that.paper.rect(x * that.options.tileSize, y * that.options.tileSize, that.options.tileSize, that.options.tileSize);
+        rect.attr({fill:"#EEE","stroke-width":2,"stroke":"#222"});
+        return rect;
+      }
+      Raphael.fn.checkMarker = function(tile) {
+        var lineTwoStart = [tile.attrs.x + tile.attrs.width, tile.attrs.y];
+        var lineOne = "M" + tile.attrs.x + " " + tile.attrs.y + "L" + (tile.attrs.x + tile.attrs.width) + " " + (tile.attrs.y + tile.attrs.height);
+        var lineTwo = "M" + (tile.attrs.x + tile.attrs.width) + " " + tile.attrs.y + "L" + tile.attrs.x + " " + (tile.attrs.y + tile.attrs.height);
+        return that.paper.path(lineOne + " " + lineTwo);
+      }
+      Raphael.fn.circleMarker = function(tile) {
+        var cx = tile.attrs.x + tile.attrs.width / 2;
+        var cy = tile.attrs.y + tile.attrs.height / 2;
+        return that.paper.circle(cx, cy, that.options.tileSize / 2);
       }
     },
     initialize: function() {
@@ -16,19 +30,22 @@
       for (var i = 0; i < 3; i++) {
         board[i] = new Array(3);
       }
-      this.board = board; 
+      this.board = board;
       this.paper = Raphael(0, 0, this.options.tileSize * 3, this.options.tileSize * 3);
       this.resetGame();
     },
     resetGame: function() {
       for (var x = 0; x < 3; x++) {
         for (var y = 0; y < 3; y++) {
-          //var newTile = this.paper.rect(x * this.options.tileSize, y * this.options.tileSize, this.options.tileSize, this.options.tileSize);
           var newTile = this.paper.tile(x, y);
-          this.board[x][y] = newTile; 
+          newTile.click(function() {
+            this.paper.circleMarker(this);
+            this.paper.checkMarker(this);
+          });
+          console.log(newTile);
+          this.board[x][y] = newTile;
         }
       }
-      console.log(this.board);
     },
     checkForWinner: function(board) {
 
